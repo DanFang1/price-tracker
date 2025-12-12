4dd473733694a34de311910ba49e9d7d53c5cb21
DanFang1 <Dannyfang2007@gmail.com>
2025-12-11 21:07:09 -0500

    Add authentication functions for user registration and login

--- Backend/auth.py
+++ Backend/auth.py
@@ -6,7 +6,7 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def hash_password(password: str) -> str:
     "Hashes a plain text password using bcrypt"
-    return pwd_context.hash(password)
+    return pwd_context.hash(password.encode('utf-8'))
 
 
 def verify_password(plain_password: str, hashed_password:str) -> bool:
@@ -20,7 +20,7 @@ def register_user(username: str, email: str, password: str) -> int:
 
     query = sql.SQL(
         """
-        INSERT INTO accounts (username, email, hash_password)
+        INSERT INTO accounts (username, hash_password, email)
         VALUES (%s, %s, %s)
         RETURNING user_id;
         """
@@ -29,7 +29,7 @@ def register_user(username: str, email: str, password: str) -> int:
     try:
         with get_connection() as conn:
             with conn.cursor() as cur:
-                cur.execute(query, (username, email, hashed_pwd))
+                cur.execute(query, (username, hashed_pwd, email))
                 user_id = cur.fetchone()[0]
                 conn.commit()
         return user_id 
@@ -55,8 +55,25 @@ def login_user(username: str, password: str) -> bool:
 
     if not verify_password(password, hashed_password):
         raise ValueError("Password is incorrect.")
-    
+
     return user_id
     
+if __name__ == "__main__":
+    from database import check_connection
+    check_connection()
+
+    try:
+        uid = register_user("ashwini", "ashwini@example.com", "ashwinipass6769")
+        print(f"Registered user {uid}")
+    except ValueError as e:
+        print(f"Registered failed: {e}")
+
+    try:
+        uid = login_user("ashwini", "ashwinipass6769")
+        print(f"User {uid} logged in successfully.")
+    except ValueError as e:
+        print(f"Login failed: {e}")
+
+        
 
 
